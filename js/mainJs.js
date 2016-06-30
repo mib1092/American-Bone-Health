f67b7be56fc0d5e38c82cec705dacba9444a83ae
mdwork <mazdze@gmail.com>
2016-06-30 20:38:39 +0300

    mib

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,33 +1,57 @@
 $(document).ready(function() {
-    //блокировка стандартного поведения ссылок
-    function prevent(){
-        $('.prevent, a[href="#"]').on('click', function(event){
-            event.preventDefault();
-        });
-    }
-    prevent();
 
     // scroll navigation fixed
-    $(window).scroll(function(){
-        $('.header').toggleClass('scroll-js', $(this).scrollTop() > 0);
-    });
+    if (document.body.clientWidth > '991') {
+        $(window).scroll(function(){
+            $('.header').toggleClass('scroll-js', $(this).scrollTop() > 0);
+        });
+    }
 
     // for burger menu
-    $(".mobile-menu-toggle, .mobile-menu-overlay").on('click', function(){
+    $(".mobile-menu-toggle, .mobile-menu-overlay, .donate-mobile .donate").on('click', function(){
         $('.mobile-menu-toggle').toggleClass('active');
         $('.mobile-menu-wrap').toggleClass('showing');
         $('body').toggleClass('overflow');
     });
     $(".mobile-menu a").on('click', function(){
-        $(this).siblings('.sub-menu').slideToggle(350);
+        $(this).toggleClass('open').siblings('.sub-menu').slideToggle(350);
     });
-    $(".mobile-menu a + .sub-menu").on('load', function(){
-        $(this).siblings('a').toggleClass('expanded');
+    $(window).on('load', function(){
+        $(".mobile-menu a + .sub-menu").siblings('a').toggleClass('expanded');
     });
 
     // for header search toggle
     $(".search-btn, .close-search").on('click', function(){
         $('#search-box').slideToggle(300);
+    });
+
+    // the blocking of links default behavior
+    function prevent(){
+        $('.prevent, .mobile-menu .expanded, a[href="#"]').on('click', function(event){
+            event.preventDefault();
+        });
+    }
+    prevent();
+
+    // for "Faces of Osteoporosis" Section on devices
+    if (document.body.clientWidth <= '991') {
+        $('.faces-wrap').on("click", function() {
+            location.href = $(this).attr('data-href');
+        });
+
+        if (document.body.clientWidth <= '768') {
+            $(window).on('load resize', function() {
+                $('.faces-list li:not(.small-faces)').each(function(index){
+                    var face = $('.faces-list li:not(.small-faces)').eq(index);
+                    face.css({'height': face.width()});
+                });
+            });
+        }
+    }
+
+    // for FAQ accordion
+    $(".accordion-list > li").on('click', function(){
+        $(this).toggleClass('active').find('.accordion-box-content').slideToggle(500);
     });
 
     // for Owl Carousel sliders
@@ -54,8 +78,4 @@ $(document).ready(function() {
         }
     });
 
-    // for FAQ accordion
-    $(".accordion-list > li").on('click', function(){
-        $(this).toggleClass('active').find('.accordion-box-content').slideToggle(500);
-    });
 });
